774e2e24bcee5b8fed988215b6def53347a9d762
Istvan <steven.ford.j@gmail.com>
2021-09-22 16:00:00 +0300

    fx

--- src/server.js
+++ src/server.js
@@ -17,8 +17,19 @@ const port = process.env.PORT;
 const publicFolderPath = join(process.cwd(), "public");
 // === COnfiguration | Before endpoints! ===
 server.use(express.static(publicFolderPath));
+// === Serve CORS ===
+const whiteList = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+const corsOptions = {
+  origin: function (origin, next) {
+    if (!origin || whiteList.indexOf(origin) != -1) {
+      next(null, true);
+    } else {
+      next(new Error("Origin not allowed!"));
+    }
+  },
+};
 // body converter
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(express.json());
 // ==== ROUTES / ENDPOINTS ====
 server.use("/authors", authorStrive);
